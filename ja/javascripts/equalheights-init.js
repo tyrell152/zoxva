3f04e47ed17949f447e59c50a556540620a9e951
Tyrell <tyrell152@gmail.com>
2018-04-10 17:49:12 -0400

    yup

---
+++ ja/javascripts/equalheights-init.js
@@ -0,0 +1,45 @@
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+			
+		    //EqualHeights triggering
+		    $('.equal-height').setAllToMaxHeight();
+		    $('.about-content-inner').setAllToMaxHeight();
+		    $('.service-item').setAllToMaxHeight();
+		    $('.team-info, .team-thumb').setAllToMaxHeight();
+		    $('.contact-content-inner').setAllToMaxHeight();
+		    $('.footer-column').setAllToMaxHeight();
+		    $('.about-content').setAllToMaxHeight();
+		    $('.news-post-inner').setAllToMaxHeight();
+		    $('.error-content-inner').setAllToMaxHeight();
+						
+		
+        
+   
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+  
+
